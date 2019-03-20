e5de08267f930052d10ddcf3517b271385257f97
CalamarFrit <amelieluquetc@gmail.com>
2019-03-20 15:06:20 +0100

    Configuration de la classe Maison.java

--- src/Entities/Maison.java
+++ src/Entities/Maison.java
@@ -1,5 +1,30 @@
 package Entities;
 
-public class Maison {
+import java.util.ArrayList;
+import util.Position;
+import util.Surface;
 
+public class Maison implements Lieu {
+	//Attributs
+	
+	//Constructeurs
+	
+	//Méthodes
+	
+	
+	@Override
+	public Position getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<Lieu> getVoisins() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Surface getSurface() {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 }
