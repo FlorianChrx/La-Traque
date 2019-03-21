6be645c9df6640726b84c3d5ba467755ef1173da
FlorianChrx <florianchiro@hotmail.fr>
2019-03-21 16:39:58 +0100

    Commentaires Lieu.java

--- src/Structures/Lieu.java
+++ src/Structures/Lieu.java
@@ -13,12 +13,50 @@ import util.Surface;
  */
 
 public interface Lieu {
+	/**
+	 * Permet d'obtenir la Position du lieu
+	 * @return Retourne la position du lieu
+	 */
 	public Position getPosition();
+	/**
+	 * Permet de redéfinir la position du lieu
+	 * @param s correspondant à une position
+	 */
 	public void setPosition(Position s);
+	/**
+	 * Permet de redéfinir la position du lieu
+	 * @param x correspondant à une abscisse
+	 * @param y correspondant à une ordonnée
+	 */
 	public void setPosition(int x, int y);
+	/**
+	 * Permet d'obtenir les lieux voisins accessibles
+	 * @return Retourne liste de lieus
+	 */
 	public ArrayList<Lieu> getVoisins();
+	/**
+	 * Permet d'obtenir la surface du lieu
+	 * @return Retourne une surface
+	 */
 	public Surface getSurface();
+	/**
+	 * Permet de redéfinir la surface du lieu
+	 * @param s correspondant à une surface
+	 */
 	public void setSurface(Surface s);
-	public void setSurface(Position s, int height, int width);
+	/**
+	 * Permet de redéfinir la surface du lieu
+	 * @param p correspondant à une position d'origine
+	 * @param height correspondant à une hauteur
+	 * @param width correspondant à une largeur
+	 */
+	public void setSurface(Position p, int height, int width);
+	/**
+	 * Permet de redéfinir la surface du lieu
+	 * @param x correspondant à l'abscisse de l'origine
+	 * @param y correspondant à l'ordonnée de l'origine
+	 * @param height correspondant à une hauteur
+	 * @param width correspondant à une largeur
+	 */
 	public void setSurface(int x, int y, int height, int width);
 }
