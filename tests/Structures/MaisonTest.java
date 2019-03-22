5729baf1739406a833fd2cb06136be91aecc2249
FlorianChrx <florianchiro@hotmail.fr>
2019-03-22 20:39:14 +0100

    Update MaisonTest

--- tests/Structures/MaisonTest.java
+++ tests/Structures/MaisonTest.java
@@ -12,9 +12,18 @@ import util.Position;
 import util.Surface;
 
 class MaisonTest {
+	
+	private static Village village;
+	private static Statut statut;
+	private static Surface surface;
+	private static Maison maison;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		village = new Village();
+		statut = Statut.ALIVE;
+		surface = new Surface(5, 5, 20, 20);
+		maison = new Maison(village, statut, surface);
 	}
 
 	@AfterAll
@@ -23,6 +32,10 @@ class MaisonTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		maison.setSurface(surface);
+		maison.setStatut(statut);
+		maison.setVillage(village);
+		maison.resetDays();
 	}
 
 	@AfterEach
@@ -31,68 +44,49 @@ class MaisonTest {
 
 	@Test
 	void testMaisonVillageStatutSurface() {
-		Village village = new Village();
-		Statut statut = Statut.ALIVE;
-		Surface surface = new Surface(5, 5, 20, 20);
-		Maison maison = new Maison(village, statut, surface);
-		assertEquals(village, maison.getVillage());
-		assertEquals(statut, maison.getStatut());
-		assertEquals(surface, maison.getSurface());
+		Maison maisonT = new Maison(village, statut, surface);
+		assertEquals(village, maisonT.getVillage());
+		assertEquals(statut, maisonT.getStatut());
+		assertEquals(surface, maisonT.getSurface());
 	}
 
 	@Test
 	void testMaisonVillageStatutPositionIntInt() {
-		Village village = new Village();
-		Statut statut = Statut.ALIVE;
-		Surface surface = new Surface(0, 0, 20, 20);
-		Maison maison = new Maison(village, statut, new Position(0, 0), 20, 20);
-		assertEquals(village, maison.getVillage());
-		assertEquals(statut, maison.getStatut());
-		assertEquals(surface, maison.getSurface());
+		Maison maisonT = new Maison(village, statut, new Position(5, 5), 20, 20);
+		assertEquals(village, maisonT.getVillage());
+		assertEquals(statut, maisonT.getStatut());
+		assertEquals(surface, maisonT.getSurface());
 	}
 
 	@Test
 	void testMaisonVillagePositionIntInt() {
-		Village village = new Village();
-		Statut statut = Statut.ALIVE;
-		Surface surface = new Surface(7, 7, 20, 20);
-		Maison maison = new Maison(village, new Position(7, 7), 20, 20);
-		assertEquals(village, maison.getVillage());
-		assertEquals(statut, maison.getStatut());
-		assertEquals(surface, maison.getSurface());
+		Maison maisonT = new Maison(village, new Position(5, 5), 20, 20);
+		assertEquals(village, maisonT.getVillage());
+		assertEquals(statut, maisonT.getStatut());
+		assertEquals(surface, maisonT.getSurface());
 	}
 
 	@Test
 	void testMaisonVillageIntIntIntInt() {
-		Village village = new Village();
-		Statut statut = Statut.ALIVE;
-		Surface surface = new Surface(5, 5, 20, 20);
-		Maison maison = new Maison(village, 5, 5, 20, 20);
-		assertEquals(village, maison.getVillage());
-		assertEquals(statut, maison.getStatut());
-		assertEquals(surface, maison.getSurface());
+		Maison maisonT = new Maison(village, 5, 5, 20, 20);
+		assertEquals(village, maisonT.getVillage());
+		assertEquals(statut, maisonT.getStatut());
+		assertEquals(surface, maisonT.getSurface());
 	}
 
 	@Test
 	void testGetPosition() {
-		Maison maison = new Maison(new Village(), 5, 5, 20, 20);
 		assertEquals(new Position(5,5), maison.getPosition());
 	}
 
 	@Test
-	void testGetVoisins() {
-	}
-
-	@Test
 	void testGetSurface() {
-		Maison maison = new Maison(new Village(), 5, 5, 20, 20);
 		assertEquals(new Surface(5,5,20,20), maison.getSurface());
 	}
 
 	@Test
 	void testSetPositionPosition() {
 		Position position = new Position(5,5);
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setPosition(position);
 		assertEquals(position, maison.getPosition());
 	}
@@ -100,7 +94,6 @@ class MaisonTest {
 	@Test
 	void testSetPositionIntInt() {
 		Position position = new Position(5,5);
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setPosition(5,5);
 		assertEquals(position, maison.getPosition());
 	}
@@ -108,7 +101,6 @@ class MaisonTest {
 	@Test
 	void testSetSurfaceSurface() {
 		Surface surface = new Surface(5,5, 15, 15);
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setSurface(surface);
 		assertEquals(surface, maison.getSurface());
 	}
@@ -116,7 +108,6 @@ class MaisonTest {
 	@Test
 	void testSetSurfacePositionIntInt() {
 		Surface surface = new Surface(5,5, 15, 15);
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setSurface(new Position(5,5),15,15);
 		assertEquals(surface, maison.getSurface());
 	}
@@ -124,14 +115,12 @@ class MaisonTest {
 	@Test
 	void testSetSurfaceIntIntIntInt() {
 		Surface surface = new Surface(5,5, 15, 15);
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setSurface(5,5,15,15);
 		assertEquals(surface, maison.getSurface());
 	}
 
 	@Test
 	void testNextDay() {
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.nextDay();
 		assertEquals(1, maison.getDays());
 		maison.nextDay();
@@ -146,14 +135,12 @@ class MaisonTest {
 
 	@Test
 	void testSetDead() {
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setDead();
 		assertEquals(Statut.DEAD, maison.getStatut());
 	}
 
 	@Test
 	void testSetAlive() {
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setDead();
 		maison.setAlive();
 		assertEquals(Statut.ALIVE, maison.getStatut());
@@ -161,33 +148,35 @@ class MaisonTest {
 
 	@Test
 	void testSetMurder() {
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setMurder();
 		assertEquals(Statut.MURDER, maison.getStatut());
 	}
 
 	@Test
 	void testSetInvestigate() {
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setInvestigate();
 		assertEquals(Statut.INVESTIGATE, maison.getStatut());
 	}
 
 	@Test
 	void testSetStatut() {
-		Maison maison = new Maison(new Village(), 10, 10, 20, 20);
 		maison.setStatut(Statut.INVESTIGATE);
 		assertEquals(Statut.INVESTIGATE, maison.getStatut());
 	}
 
 	@Test
 	void testEqualsObject() {
-		Village village = new Village();
-		Maison maison = new Maison(village, 10, 10, 20, 20);
-		Maison maison2 = new Maison(village, 10, 10, 20, 20);
-		Maison maison3 = new Maison(village, 5, 5, 20, 20);
+		Maison maison2 = new Maison(village, 5, 5, 20, 20);
+		Maison maison3 = new Maison(village, 10, 10, 20, 20);
 		assertTrue(maison.equals(maison2));
-		assertFalse(maison2.equals(maison3));
+		assertFalse(maison.equals(maison3));
+	}
+	
+	@Test
+	void testResetDays() {
+		maison.nextDay();
+		maison.resetDays();
+		assertEquals(0, maison.getDays());
 	}
 
 }
