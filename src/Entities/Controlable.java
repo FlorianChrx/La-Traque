1463a38c320bf4d83e6b44aa5c0a3e85491870a5
FlorianChrx <florianchiro@hotmail.fr>
2019-03-22 21:37:06 +0100

    Update Controlable.java
    Documentation

--- src/Entities/Controlable.java
+++ src/Entities/Controlable.java
@@ -1,10 +1,24 @@
 package Entities;
 
 import java.util.ArrayList;
-
 import Structures.Lieu;
 
+
+/**
+ * 
+ * Interface énumérant les fonctions de chaque entité contrôlable
+ * @author florianchiraux
+ *
+ */
 public interface Controlable {
+	/**
+	 * Permet de lister les lieux accessible à l'entité
+	 * @return une liste de lieu où peut se rendre l'entité
+	 */
 	public ArrayList<Lieu> lieuxAccessibles();
+	/**
+	 * Permet de déplacer l'entité
+	 * @param lieu représentant le lieu où doit se rendre l'entité
+	 */
 	public void  goTo(Lieu lieu);
 }
