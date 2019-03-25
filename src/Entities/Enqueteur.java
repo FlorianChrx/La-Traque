64dd492e00dd87766314c6c88484af9539906c0c
FlorianChrx <florianchiro@hotmail.fr>
2019-03-25 13:26:06 +0100

    Update Enqueteur

--- src/Entities/Enqueteur.java
+++ src/Entities/Enqueteur.java
@@ -1,14 +1,20 @@
 package Entities;
 
-import java.util.ArrayList;
-
 import Structures.Lieu;
+import Structures.Village;
 
 public abstract class Enqueteur extends Personnage {
-	//Attributs
+
+	public Enqueteur(Village village, int nbActions) {
+		super(village, nbActions);
+	}
+	public Enqueteur(Lieu lieu, int nbActions) {
+		super(lieu, nbActions);
+	}
 	
-	//Constructeurs
-	
-	//Méthodes
-	
+	public void enquete() {
+		System.out.println(lieu.getPhrase());
+	}
+
 }
+
