4ca66b353aa2706f69b9199b69207d096b68e67e
GeoJo <vaniscotte.geoffrey@hotmail.com>
2019-03-25 13:56:43 +0100

    Commencement de la class Tueur

--- src/Entities/Tueur.java
+++ src/Entities/Tueur.java
@@ -1,15 +1,40 @@
 package Entities;
 
-import java.util.ArrayList;
-
 import Structures.Lieu;
+import Structures.Maison;
+import Structures.Village;
 
+/**
+ * Class abstraite correspondant aux Tueurs
+ * @author geoffrey
+ *
+ */
 public abstract class Tueur extends Personnage {
-
-	//Attributs
 	
-	//Constructeurs
+	/**
+	 * Constructeur avec un village en prenant une maison aléatoirement comme lieu de départ
+	 * @param village
+	 * @param nbActions que peut effectuer le personnage
+	 */
+	public Tueur(Village village, int nbActions) {
+		super(village, nbActions);
+	}
 	
-	//Méthodes
+	/**
+	 * Construteur avec le lieu de départ du tueur
+	 * @param lieu de départ
+	 * @param nbActions que peut effectuer le personnage
+	 */
+	public Tueur(Lieu lieu, int nbActions) {
+		super(lieu, nbActions);
+	}
 	
+	/**
+	 * Permet de tuer le résident d'une maison
+	 */
+	public void tue() {
+		if(lieu instanceof Maison) {
+			((Maison) lieu).setDead();
+		}
+	}
 }
