eb709f2edbc8a331493101e06f78003a48178c24
FlorianChrx <florianchiro@hotmail.fr>
2019-03-28 19:47:15 +0100

    Update Village.java

--- src/Structures/Village.java
+++ src/Structures/Village.java
@@ -1,6 +1,8 @@
 package Structures;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 import Entities.Enqueteur;
 import Entities.Tueur;
 
@@ -10,6 +12,22 @@ public class Village {
 	private Tueur tueur;
 	private Enqueteur enqueteur;
 	
+	public Village(Tueur tueur, Enqueteur enqueteur, String lienCSV) {
+		init(lienCSV);
+		Collections.shuffle(lieux);
+		this.tueur = tueur;
+		this.tueur.setLieu(lieux.get(0));
+		this.enqueteur = enqueteur;
+		this.enqueteur.setLieu(lieux.get(1));
+	}
+
+	private void init(String lienCSV) {
+		this.lieux = new ArrayList<Lieu>();
+		// à développer
+		this.routes = new Routes(lieux);
+		// à développer
+	}
+
 	public ArrayList<Lieu> getVoisins(Lieu lieu) {
 		return routes.getVoisins(lieu);
 	}
