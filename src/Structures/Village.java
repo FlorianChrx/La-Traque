cdcee3e334a4b6bd9588dfa38a29758ce1821323
Florian Chiraux <43887259+FlorianChrx@users.noreply.github.com>
2019-03-30 00:06:50 +0100

    Florian (#15)
    * Update VIllage
    
    Ajout de méthode permettant de représenter le village par une chaine de caractère
    
    * Update Village
    
    Ajout des commentaires aux méthodes créées
    
    * Update MaisonTest
    
    Correction du probleme de constructeur dans les test de maison
    
    * Revert "Update MaisonTest"
    
    This reverts commit 6464542931f26b2ad71a26b9772b1f28fd546f08.

--- src/Structures/Village.java
+++ src/Structures/Village.java
@@ -24,4 +24,35 @@ public class Village {
 	public ArrayList<Lieu> getLieux() {
 		return lieux;
 	}
+	/**
+	 * Méthode permettant de transformer le village en tableau de booleen (les cases avec la valeur vrai étant 
+	 * des cases avec une maison à cette position)
+	 * /!\ la map doit etre inferieure ou égale à 30*30 pour appeler cette méthode sous peine de faire 
+	 * planter le jeu
+	 */
+	public boolean[][] toBooleanTab(){
+		boolean[][] tab = new boolean[30][30];
+		for (Lieu lieu : lieux) {
+			tab[lieu.getPosition().getX()][lieu.getPosition().getY()] = true;
+		}
+		return tab;
+	}
+	/**
+	 * Méthode permettant de représenter le village par une chaine de caractères à partir d'un tableau booléen
+	 * généré à l'appel de cette méthode via la méthode {@link toBooleanTab()}
+	 */
+	public String toString() {
+		boolean[][] tab = toBooleanTab();
+		String res = "";
+		for (int i = 0; i < tab.length; i++) {
+			for (int j = 0; j < tab[i].length; j++) {
+				if (tab[i][j]) {
+					res += "#";
+				} else {
+					res += ".";
+				}
+			}
+		}
+		return res;
+	}
 }
