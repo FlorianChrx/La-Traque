ec7a2da3897ed7cb8c3b01323a366c43673c2dfc
FlorianChrx <florianchiro@hotmail.fr>
2019-03-30 11:38:43 +0100

    Update Statut.java

--- src/Structures/Statut.java
+++ src/Structures/Statut.java
@@ -1,5 +1,14 @@
 package Structures;
 
+/**
+ * 
+ * Classe enumérant les status possibles pour un lieu
+ * @author florianchiraux
+ *
+ */
 public enum Statut {
+	/**
+	 * Statuts possibles pour un lieu
+	 */
 	 MURDER, INVESTIGATE, EMPTY;
 }
