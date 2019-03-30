a3a40807a81a431619f1f620ffcc0324a6d2ef74
FlorianChrx <florianchiro@hotmail.fr>
2019-03-30 15:34:45 +0100

    Update Game.java

--- src/util/Game.java
+++ src/util/Game.java
@@ -14,13 +14,28 @@ public class Game {
 	private static Tueur tueur;
 	private static Enqueteur enqueteur;
 	private static Scanner clavier = new Scanner(System.in);
+	private static boolean win = false;
 	
 	public static void main(String[] args) {
 		System.out.println("Bienvenue dans notre jeu appuies sur entrée pour jouer !");
 		clavier.nextLine();
-		villageActuel = new Village("");
+		villageActuel = new Village("../../ressources/empty.csv");
 		tueur = null;
 		enqueteur = new MaitreChien(villageActuel);
+		while (!win) {
+			 String choix;
+			 int choixInt;
+			 System.out.println(villageActuel.toString());
+			 System.out.println(villageActuel.getVoisinsString(getEnqueteurLocation()));
+			 System.out.println("Votre Choix :");;
+			 do {
+				 choix = clavier.nextLine();
+				 // - 1 pour avoir l'indice de la maison choisie (car le chiffre visible
+				 // est le numéro de la maison et non son indice)
+				 choixInt = Integer.parseInt(choix) - 1;
+			 } while (choixInt > villageActuel.getLieux().size() || ! enqueteur.canGoTo(villageActuel.getLieux().get(choixInt)));
+			 enqueteur.goTo(villageActuel.getLieux().get(choixInt));
+		}
 	}
 	
 	public static Lieu getTueurLocation() {
@@ -38,8 +53,13 @@ public class Game {
 	public static Enqueteur getEnqueteur() {
 		return enqueteur;
 	}
-
+	
 	public static Village getVillageActuel() {
 		return villageActuel;
 	}
+
+	public static void win() {
+		win = true;
+	}
+	
 }
