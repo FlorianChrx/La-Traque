b712d00627cc1fa1e6d5a0a0f55a8fe5a54d1dea
FlorianChrx <florianchiro@hotmail.fr>
2019-03-30 18:30:03 +0100

    Update StatutMaison.java

--- src/Structures/StatutMaison.java
+++ src/Structures/StatutMaison.java
@@ -1,10 +1,15 @@
 package Structures;
 /**
  * 
- * Classe énumérant les status possibles d'une maison
+ * Classe énumérant les status possibles d'une maison (à ne pas confondre avec le statut d'un lieu)
  * @author florianchiraux
  *
  */
 public enum StatutMaison {
+	/**
+	 * Statut disponibles pour une maison
+	 * ALIVE: l'habitant est vivant
+	 * DEAD: l'habitant est mort
+	 */
 	ALIVE, DEAD;
 }
