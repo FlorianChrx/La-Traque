cc5467de813adc607b94a7b82c06e9acf266fa08
FlorianChrx <florianchiro@hotmail.fr>
2019-03-31 12:59:34 +0200

    Update Entity.java

--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -1,5 +1,13 @@
 package Entities;
-
+/**
+ * 
+ * Classe représentant un entité présente sur le plateau
+ * @author florianchiraux
+ *
+ */
 public interface Entity {
+	/**
+	 * Permet de mettre à jour une entité (utile en fin de tour)
+	 */
 	public void update();
 }
