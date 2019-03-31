c8b81b40d124d5a10f0eba5393c5b655c3d3605c
FlorianChrx <florianchiro@hotmail.fr>
2019-03-31 22:42:38 +0200

    Update Game.java
    tour de l'enqueteur rendu quasiment entièrment opérationel (le compagnon n'est pas encore pris en compte)

--- src/util/Game.java
+++ src/util/Game.java
@@ -3,6 +3,7 @@ package util;
 import java.util.Scanner;
 
 import Entities.Enqueteur;
+import Entities.Personnage;
 import Entities.Tueur;
 import Structures.Lieu;
 import Structures.Village;
@@ -23,19 +24,9 @@ public class Game {
 		tueur = null;
 		enqueteur = new MaitreChien(villageActuel);
 		while (!win) {
-			 String choix;
-			 int choixInt;
-			 System.out.println(villageActuel.toString());
-			 System.out.println(villageActuel.getVoisinsString(getEnqueteurLocation()));
-			 do {
-				 System.out.println("-- Tour de l'enqueteur --");
-				 System.out.println("Votre Choix :");
-				 choix = clavier.nextLine();
-				 // - 1 pour avoir l'indice de la maison choisie (car le chiffre visible
-				 // est le numéro de la maison et non son indice)
-				 choixInt = Integer.parseInt(choix);
-			 } while ((choixInt > villageActuel.getLieux().size() && choixInt < villageActuel.getLieux().size() || !enqueteur.canGoTo(villageActuel.getLieux().get(choixInt-1)) && !enqueteur.getLieu().equals(villageActuel.getLieux().get(choixInt-1))) && enqueteur.canDoAction());
-			 enqueteur.action(villageActuel.getLieux().get(choixInt-1));
+			do {
+				 tourEnqueteur();
+			 } while(enqueteur.canDoAction());
 			 /*
 			 do {
 				 System.out.println("-- Tour du tueur --");
@@ -47,9 +38,50 @@ public class Game {
 			 } while ((choixInt > villageActuel.getLieux().size() && choixInt < villageActuel.getLieux().size() || !tueur.canGoTo(villageActuel.getLieux().get(choixInt-1))) && tueur.canDoAction());
 			 tueur.action(villageActuel.getLieux().get(choixInt-1));
 			 */
+			 System.out.println("Jour suivant !");
+			 clavier.nextLine();
+			 enqueteur.update();
 		}
 	}
 	
+	public static void afficherVillagePour(Personnage p) {
+		System.out.println(villageActuel.toString());
+		System.out.println(villageActuel.getVoisinsString(p.getLieu()));
+	}
+	
+	public static void tourEnqueteur() {
+		afficherVillagePour(enqueteur);
+		int choix;
+		System.out.println("-- Tour de l'enqueteur --");
+		System.out.println(enqueteur.getActionsDone());
+		System.out.println(enqueteur.getNbActions());
+
+		System.out.println("Votre Choix :");
+		choix = choix(1, villageActuel.getLieux().size()+1);
+		if (choix != -1 && (enqueteur.canGoTo(villageActuel.getLieux().get(choix-1)) || enqueteur.getLieu().equals(villageActuel.getLieux().get(choix-1)))) {
+			enqueteur.action(villageActuel.getLieux().get(choix-1));
+		}
+	}
+	
+	public static int choix(int min, int max) {
+		String choix;
+		do {
+			choix = clavier.nextLine();
+		} while(!isNumeric(choix));
+		int res = Integer.parseInt(choix);
+		if(res >= max || res < min) return -1;
+		return res;
+	}
+	
+	public static boolean isNumeric(String choix) {
+		try {
+            Integer.parseInt(choix);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+	}
+
 	public static Lieu getTueurLocation() {
 		return tueur.getLieu();
 	}
