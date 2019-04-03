add42ce685574d8dec6652857c4abd55c88fcfe8
Baptiste Carion <baptiste.carion@sfr.fr>
2019-04-03 14:02:10 +0200

    Update phrase : ajout nomMaison

--- src/Structures/Phrase.java
+++ src/Structures/Phrase.java
@@ -1,27 +1,36 @@
 package Structures;
 
 import java.util.List;
+import util.CSVReader;
 import java.util.Random;
 
-import util.CSVReader;
-
 public class Phrase {
-	private List<String> courts;
-	private List<String> moyens; 
-	private List<String> longs; 
-	private Random alea;
+	private static List<String> courts;
+	private static List<String> moyens; 
+	private static List<String> longs; 
+	private static List<String> noms;
+	private static Random alea;
 	
 	public Phrase() {
-		CSVReader court = new CSVReader("../../DATA/court.csv");
-		CSVReader moyen = new CSVReader("../../DATA/moyen.csv");
-		CSVReader longss = new CSVReader("../../DATA/long.csv");
+		CSVReader court = new CSVReader("court.csv");
+		CSVReader moyen = new CSVReader("moyen.csv");
+		CSVReader longss = new CSVReader("long.csv");
+		CSVReader nomss = new CSVReader("noms.csv");
 		courts = court.getList();
 		moyens = moyen.getList();
 		longs = longss.getList();
+		noms = nomss.getList();
 		alea = new Random();
 	}
 	
-	public String nextPhrase(int nbJours) {
+	public static String nomMaison() {
+		int i = alea.nextInt(noms.size());
+		String nom = noms.get(i);
+		noms.remove(i);
+		return nom;
+	}
+	
+	public static String nextPhrase(int nbJours) {
 		if (nbJours < 3) {
 			return nextPhraseShort();
 		}
@@ -30,15 +39,15 @@ public class Phrase {
 		}
 		return nextPhraseLong();
 	}
-
-	public String nextPhraseShort() {
+	
+	public static String nextPhraseShort() {
 		return courts.get(alea.nextInt(40));
 	}
 	
-	public String nextPhraseMedium() {
+	public static String nextPhraseMedium() {
 		return moyens.get(alea.nextInt(40));
 	}
-	public String nextPhraseLong() {
+	public static String nextPhraseLong() {
 		return longs.get(alea.nextInt(40));
 	}
 }
