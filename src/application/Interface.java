6c4da5c1a56047e9300fab0de80322fbc9ad803e
FlorianChrx <florianchiro@hotmail.fr>
2019-06-10 20:00:53 +0200

    patch
    - Interface
    - SController

--- src/application/Interface.java
+++ src/application/Interface.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
 public enum Interface {
 	MAINMENU("interface_first.fxml"), MODEMENU("interface_nbJoueur.fxml"), CHOIXMENU("interface.fxml"), ENQUETEURMENU("interfaceChoixEnqueteur.fxml"), TUEURMENU("interfaceChoixTueur.fxml");
 	private String link;
@@ -8,5 +13,10 @@ public enum Interface {
 	}
 	public String getLink() {
 		return link;
+	}
+	public Scene newScene() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource(getLink()));
+		return new Scene(loader.load());
 	}
 }
